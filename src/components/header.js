f1c9b42d210dccd765c84872440f60dfda426372
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-02-15 19:23:24 -0500

    create header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./components.css";
+
+const Header = () => {
+  return (
+    <div className="header-div">
+      <h2> Memory Card</h2>
+    </div>
+  );
+};
+
+export default Header;
