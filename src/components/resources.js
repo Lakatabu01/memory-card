89b4b8a93e405f3390484ac1b467ebd34200af9c
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-02-16 08:33:03 -0500

    create resource component

---
+++ src/components/resources.js
@@ -0,0 +1,23 @@
+import fish1 from "./images/fish1";
+import fish2 from "./images/fish2";
+import fish3 from "./images/fish3";
+import fish4 from "./images/fish4";
+import fish5 from "./images/fish5";
+import fish6 from "./images/fish6";
+import fish7 from "./images/fish7";
+import fish8 from "./images/fish8";
+import fish9 from "./images/fish9";
+import fish10 from "./images/fish10";
+
+export const fishGroup = [
+  { src: fish1, id: 1, name: "Pink Maomao" },
+  { src: fish2, id: 2, name: "Butterfly Fish" },
+  { src: fish3, id: 3, name: "Lion Fish" },
+  { src: fish4, id: 4, name: "Blue Strped Grunt" },
+  { src: fish5, id: 5, name: "Gold Fish" },
+  { src: fish6, id: 6, name: "Grizzle" },
+  { src: fish7, id: 7, name: "Blue Halfmoon" },
+  { src: fish8, id: 8, name: "Halfmoon" },
+  { src: fish9, id: 9, name: "Red Shadow Halfmoon" },
+  { src: fish10, id: 10, name: "Guppy" },
+];
