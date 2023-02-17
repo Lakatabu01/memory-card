25c9e35ed2d417874efe193e7e0b2526707cfbcc
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-02-17 11:47:54 -0500

    change position of cards on click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header";
+import Card from "./components/card";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card />
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { fishGroup } from "./resources";
+import { shuffle } from "./shuffle";
 
 const Card = () => {
-  const loopThroughFishGroup = fishGroup.map((fish) => (
-    <div>
-      <img src={fish.src} alt="Fish" /> <p>{fish.name}</p>{" "}
+  const [array, setArray] = useState(shuffle(fishGroup));
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    console.log("hey");
+    setArray(shuffle(fishGroup));
+  }, []);
+
+  const loopThroughFishGroup = array.map((fish) => (
+    <div
+      className="card"
+      key={fish.id}
+      onClick={() => setArray(shuffle(fishGroup))}
+    >
+      <img src={fish.src} alt="Fish" /> <p>{fish.name}</p>
     </div>
   ));
 
