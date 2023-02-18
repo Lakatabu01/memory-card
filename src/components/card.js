6eada05f2689de1e3d33628b71455e3401b0e697
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-02-18 18:36:12 -0500

    succesfully created scoreboard

--- src/components/card.js
+++ src/components/card.js
@@ -4,25 +4,53 @@ import { shuffle } from "./shuffle";
 
 const Card = () => {
   const [array, setArray] = useState(shuffle(fishGroup));
+  const [currentCard, setCurrentCard] = useState("");
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
 
+  const handleClick = (e) => {
+    setArray(shuffle(fishGroup));
+    ScoreUpdater(e);
+    highScoreUpdater(e);
+  };
+
+  //shuffles the cards when component is mounted
   useEffect(() => {
-    console.log("hey");
     setArray(shuffle(fishGroup));
   }, []);
 
+  const ScoreUpdater = (e) => {
+    if (currentCard === e.target.lastChild) {
+      setScore(0);
+    } else {
+      setScore(score + 1);
+      setCurrentCard(e.target.lastChild);
+    }
+  };
+
+  const highScoreUpdater = (e) => {
+    if (score === highScore && e.target.lastChild !== currentCard) {
+      setHighScore(highScore + 1);
+    }
+  };
+
   const loopThroughFishGroup = array.map((fish) => (
-    <div
-      className="card"
-      key={fish.id}
-      onClick={() => setArray(shuffle(fishGroup))}
-    >
-      <img src={fish.src} alt="Fish" /> <p>{fish.name}</p>
+    <div className="card" key={fish.id} onClick={handleClick}>
+      <img src={fish.src} alt="Fish" />
+      <p>{fish.name}</p>
     </div>
   ));
 
-  return <div>{loopThroughFishGroup}</div>;
+  return (
+    <div>
+      <div>
+        <div>Score: {score}</div>
+        <div>highScore: {highScore}</div>
+      </div>
+
+      {loopThroughFishGroup}
+    </div>
+  );
 };
 
 export default Card;
