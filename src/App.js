edbc8182016896e1c39e303dab42e0ca7044fa9c
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-02-21 07:38:42 -0500

    complete project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Card from "./components/card";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Card />
+      <Footer />
     </div>
   );
 }
